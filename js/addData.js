76e02405ac188c5705952b9dedf37d8c4d814259
jeffcohenn <jeff.cohen92@live.com>
2018-11-16 10:30:46 -0800

    timestamp

--- js/addData.js
+++ js/addData.js
@@ -3,7 +3,7 @@ const form = document.querySelector('#add-user-input');
 form.addEventListener('submit', function(evt) {
 	evt.preventDefault(); 
 	db.collection('userInput').add({
-		// created
+	createdAt: firebase.firestore.FieldValue.serverTimestamp(),
 		name: form.name.value, 
 		email: form.email.value, 
 		phone: form.phone.value,
