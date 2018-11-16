74e253f2cda54625f3bd87759e21dca74b46483c
jeffcohenn <jeff.cohen92@live.com>
2018-11-16 10:29:03 -0800

    phonev3

--- js/addData.js
+++ js/addData.js
@@ -3,15 +3,16 @@ const form = document.querySelector('#add-user-input');
 form.addEventListener('submit', function(evt) {
 	evt.preventDefault(); 
 	db.collection('userInput').add({
+		// created
 		name: form.name.value, 
-		phone: form.phone.value,
 		email: form.email.value, 
+		phone: form.phone.value,
 		message: form.message.value,
 		
 });
 	form.name.value = '';
-	form.phone.value = '';
 	form.email.value = '';
+	form.phone.value = '';
 	form.message.value = '';
 	
 })
